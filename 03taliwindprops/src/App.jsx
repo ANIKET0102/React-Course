00e6bc413313246301515dacc511854790e21b9f
Aniket Pawar <anipawar9028@gmai.com>
2026-01-12 23:41:40 +0530

    tailwind css and dom

---
+++ 03taliwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Error404 from "./components/error404";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Error404 />
+    </>
+  );
+}
+
+export default App;
